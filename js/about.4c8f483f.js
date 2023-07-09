fa29781a2748e88e78dc970b6e10d3f8a589e18f
nandosti <fernandosmendesti@gmail.com>
2023-07-09 13:56:02 -0300

    pages

---
+++ js/about.4c8f483f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksmart_system"]=self["webpackChunksmart_system"]||[]).push([[443],{833:function(s,t,n){n.r(t),n.d(t,{default:function(){return l}});var a=n(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),u=[r];function c(s,t){return(0,a.wg)(),(0,a.iD)("div",e,u)}var i=n(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.4c8f483f.js.map
